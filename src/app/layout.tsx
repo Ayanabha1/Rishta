fcd544c8781909b2a9c89e2bf41162e845f56c29
Ayanabha Misra <ayanabha2002@gmail.com>
2025-01-03 23:12:41 +0530

    changes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -22,7 +22,7 @@ export default function RootLayout({
       <body
         className={`${poppins.className} antialiased flex min-h-screen w-full items-center justify-center bg-gray-900 p-4`}
       >
-        <main className="relative  h-screen  max-w-[390px]  w-full overflow-hidden  bg-gradient p-4 flex">
+        <main className="relative h-screen max-w-[480px] w-full overflow-hidden  bg-gradient p-4 flex">
           {children}
           <ToastContainer
             className="absolute z-50 top-[90%]"
